920bf0f3310392049be41bfb6f7cc0f71dbf4bcc
Lahiru De Silva <lahirude@wso2.com>
2022-11-08 11:58:43 +0530

    adapter: LICENSE header changed to LLC
    Signed-off-by: Lahiru De Silva <lahirude@wso2.com>

--- adapter/internal/oasparser/model/project_structs.go
+++ adapter/internal/oasparser/model/project_structs.go
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2022, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *  Copyright (c) 2022, WSO2 LLC. (http://www.wso2.org) All Rights Reserved.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -12,6 +12,7 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
+ *
  */
 
 // This files contains models that holds the content of an API project,
