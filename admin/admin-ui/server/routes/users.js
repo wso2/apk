586e07da6c22947c506815abf5561f4ee819581a
tharindu1st <tharindua@wso2.com>
2023-01-23 17:46:24 +0530

    delete unwanted ui components

--- admin/admin-ui/server/routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resourceeezzz333');
-});
-
-module.exports = router;
