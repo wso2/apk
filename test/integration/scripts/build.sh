1de87e44c807f0aa1fbdc2c08894d1e1fdfe5426
krish <krishan.wijesena@gmail.com>
2023-03-09 08:47:10 +0530

    Remove bal

--- test/integration/scripts/build.sh
+++ test/integration/scripts/build.sh
@@ -20,15 +20,15 @@ set -e
 #This is a sample script to build APK. When you build project please run this script in project root level.
 #All relative paths etc designed from root directory. Users can customize this as per demand. Ex: If you wish to 
 #build and run runtime domain service then can build it alone and do deployment.
-mkdir -p ballerina-dist
-wget 'https://dist.ballerina.io/downloads/2201.4.0/ballerina-2201.4.0-swan-lake-linux-x64.deb' -P ballerina-dist
-sudo dpkg -i ballerina-dist/ballerina-2201.4.0-swan-lake-linux-x64.deb
+# mkdir -p ballerina-dist
+# wget 'https://dist.ballerina.io/downloads/2201.4.0/ballerina-2201.4.0-swan-lake-linux-x64.deb' -P ballerina-dist
+# sudo dpkg -i ballerina-dist/ballerina-2201.4.0-swan-lake-linux-x64.deb
 
-current_dir=$PWD;
-cd $current_dir;
-cd ../../common-bal-libs/apk-common-lib;./gradlew build;
-cd $current_dir;
-cd ../../runtime/runtime-domain-service;./gradlew build -Pversion='test';
+# current_dir=$PWD;
+# cd $current_dir;
+# cd ../../common-bal-libs/apk-common-lib;./gradlew build;
+# cd $current_dir;
+# cd ../../runtime/runtime-domain-service;./gradlew build -Pversion='test';
 cd $current_dir;
 cd ../../adapter;./gradlew build -Pversion='test';
 cd $current_dir;
