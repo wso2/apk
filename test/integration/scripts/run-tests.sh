06bab4906251ab65322be6c00a3c4aa956c805c0
krish <krishan.wijesena@gmail.com>
2023-03-09 09:12:13 +0530

    Change test script

--- test/integration/scripts/run-tests.sh
+++ test/integration/scripts/run-tests.sh
@@ -20,11 +20,10 @@
 kubectl create ns apk-integration-test
 
 # Install wso2 apk chart with cp disabled
-./helm repo add bitnami https://charts.bitnami.com/bitnami
-./helm repo add jetstack https://charts.jetstack.io
-./helm dependency build ../../helm-charts
-./helm install apk-test-setup ../../helm-charts -n apk-integration-test --set wso2.apk.cp.enabled=false \
---set dp.runtime.deployment.image=runtime-domain-service:test  --set dp.runtime.deployment.imagePullPolicy=IfNotPresent \
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo add jetstack https://charts.jetstack.io
+helm dependency build ../../helm-charts
+helm install apk-test-setup ../../helm-charts -n apk-integration-test --set wso2.apk.cp.enabled=false \
 --set dp.adapter.deployment.image=adapter:test --set dp.adapter.deployment.imagePullPolicy=IfNotPresent \
 --set dp.gatewayRuntime.deployment.router.image=router:test --set dp.gatewayRuntime.deployment.router.imagePullPolicy=IfNotPresent \
 --set dp.runtime.deployment.enforcer.image=enforcer:test --set dp.runtime.deployment.enforcer.imagePullPolicy=IfNotPresent
