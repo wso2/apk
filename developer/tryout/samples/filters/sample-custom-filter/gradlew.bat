3012535b7e2c167712fe5dbffdd77d1acb37e948
chashikajw <chashikajw007@gmail.com>
2023-03-16 02:13:37 +0530

    Restructure the code

--- developer/tryout/samples/filters/sample-custom-filter/gradlew.bat
+++ developer/tryout/samples/filters/sample-custom-filter/gradlew.bat
@@ -1,17 +1,19 @@
 @rem
-@rem Copyright 2015 the original author or authors.
+@rem Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com) All Rights Reserved.
 @rem
-@rem Licensed under the Apache License, Version 2.0 (the "License");
-@rem you may not use this file except in compliance with the License.
+@rem WSO2 LLC. licenses this file to you under the Apache License,
+@rem Version 2.0 (the "License"); you may not use this file except
+@rem in compliance with the License.
 @rem You may obtain a copy of the License at
 @rem
-@rem      https://www.apache.org/licenses/LICENSE-2.0
+@rem     http://www.apache.org/licenses/LICENSE-2.0
 @rem
-@rem Unless required by applicable law or agreed to in writing, software
-@rem distributed under the License is distributed on an "AS IS" BASIS,
-@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-@rem See the License for the specific language governing permissions and
-@rem limitations under the License.
+@rem Unless required by applicable law or agreed to in writing,
+@rem software distributed under the License is distributed on an
+@rem "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+@rem KIND, either express or implied. See the License for the
+@rem specific language governing permissions and limitations
+@rem under the License.
 @rem
 
 @if "%DEBUG%" == "" @echo off
