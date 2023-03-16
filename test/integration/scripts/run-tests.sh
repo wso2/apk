a8c472f8f1d4df8720fb0bea067ab24232b76ec1
Tharindu Dasun Dharmarathna <tharindua@wso2.com>
2023-03-16 23:24:27 +0530

    Update run-tests.sh

--- test/integration/scripts/run-tests.sh
+++ test/integration/scripts/run-tests.sh
@@ -25,8 +25,6 @@ helm repo add jetstack https://charts.jetstack.io
 helm dependency build ../../helm-charts
 helm install apk-test-setup ../../helm-charts -n apk-integration-test --set wso2.apk.cp.enabled=false \
 --set wso2.apk.dp.adapter.deployment.image=adapter:test --set wso2.apk.dp.adapter.deployment.imagePullPolicy=IfNotPresent \
---set wso2.apk.dp.gatewayRuntime.deployment.router.image=router:test \
---set wso2.apk.dp.gatewayRuntime.deployment.router.imagePullPolicy=IfNotPresent \
 --set wso2.apk.dp.gatewayRuntime.deployment.enforcer.image=enforcer:test \
 --set wso2.apk.dp.gatewayRuntime.deployment.enforcer.imagePullPolicy=IfNotPresent \
 --set wso2.apk.dp.ratelimiter.enabled=false --set wso2.apk.dp.redis.enabled=false \
