a9a8625e765f780d1ef581803d92265f374b20eb
Amila Senadheera <amila.15@cse.mrt.ac.lk>
2023-03-29 10:41:50 +0530

    wait until gateway is ready to start tests

--- test/integration/scripts/run-tests.sh
+++ test/integration/scripts/run-tests.sh
@@ -38,6 +38,7 @@ helm install apk-test-setup ../../helm-charts -n apk-integration-test --set wso2
 kubectl wait --timeout=5m -n gateway-system deployment/gateway-api-admission-server --for=condition=Available
 kubectl wait --timeout=5m -n gateway-system job/gateway-api-admission --for=condition=Complete
 kubectl wait --timeout=5m -n apk-integration-test deployment/apk-test-setup-wso2-apk-adapter-deployment --for=condition=Available
+kubectl wait --timeout=5m -n apk-integration-test deployment/apk-test-setup-wso2-apk-gateway-runtime-deployment --for=condition=Available
 kubectl describe deployment apk-test-setup-wso2-apk-adapter-deployment -n apk-integration-test
 POD=$(kubectl get pod -l networkPolicyId=adapter-npi -n apk-integration-test -o jsonpath="{.items[0].metadata.name}")
 kubectl describe pod $POD -n apk-integration-test
