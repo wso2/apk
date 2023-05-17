0a6988eef4942bf31fd736dd60e9dd7b2008a42a
Thisal Tennakoon <thisaltennakoon@gmail.com>
2023-05-17 13:29:49 +0530

    Added form validation

--- admin/admin-ui/client/source/pages/APICategories/AddUpdateAPICategory.tsx
+++ admin/admin-ui/client/source/pages/APICategories/AddUpdateAPICategory.tsx
@@ -19,6 +19,7 @@ import AddCircleOutlineRoundedIcon from '@mui/icons-material/AddCircleOutlineRou
 import EditIcon from '@mui/icons-material/Edit';
 import TextField from '@mui/material/TextField';
 import axios from 'axios';
+import Alert from "components/Alert";
 import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
 import { FormattedMessage } from 'react-intl';
@@ -26,7 +27,7 @@ import FormDialogBase from '../../components/@extended/FormDialogBase';
 
 export default function AddUpdateAPICategory({ id, nameProp, descriptionProp, updateList }) {
   // This component has been used to add API category when id is undefined and edit API category when id is defined
-  const [APICategory, setAPICategory] = useState<{name: string; description: string}>({ name: "", description: "" });
+  const [APICategory, setAPICategory] = useState<{ name: string; description: string }>({ name: "", description: "" });
 
   useEffect(() => {
     if (id !== undefined) {
@@ -34,38 +35,99 @@ export default function AddUpdateAPICategory({ id, nameProp, descriptionProp, up
     }
   }, []);
 
-  const formSaveCallback = () => {
-    if (id !== undefined) {
-      axios.put('/api/am/admin/api-categories/' + id, { 'name': APICategory.name, 'description': APICategory.description }, {
-        withCredentials: true,
-      }).then(() => {
-        return (
-          <FormattedMessage
-            id='AdminPages.ApiCategories.AddEdit.form.edit.successful'
-            defaultMessage='API Category edited successfully.'
-          />
-        );
-      }).catch((error) => {
-        throw error.response.body.description;
-      }).finally(() => {
-        updateList();
-      });
-    } else {
-      axios.post('/api/am/admin/api-categories/', { 'name': APICategory.name, 'description': APICategory.description }, {
-        withCredentials: true,
-      }).then(() => {
-        return (
-          <FormattedMessage
-            id='AdminPages.ApiCategories.AddEdit.form.add.successful'
-            defaultMessage='API Category added successfully.'
-          />
-        );
-      }).catch((error) => {
-        throw error.response.body.description;
-      }).finally(() => {
-        updateList();
-      });
+  const hasErrors = (fieldName, value) => {
+    let error;
+    switch (fieldName) {
+      case 'name':
+        if (value === undefined) {
+          error = false;
+          break;
+        }
+        if (value === '') {
+          error = 'Name is Empty';
+        } else if (value.length > 255) {
+          error = 'API Category name is too long';
+        } else if (/\s/.test(value)) {
+          error = 'Name contains spaces';
+        } else if (/[!@#$%^&*(),?"{}[\]|<>\t\n]/i.test(value)) {
+          error = 'Name field contains special characters';
+        } else {
+          error = false;
+        }
+        break;
+      case 'description':
+        if (value && value.length > 1024) {
+          error = 'API Category description is too long';
+        }
+        break;
+      default:
+        break;
     }
+    return error;
+  };
+
+  const getAllFormErrors = () => {
+    let errorText = '';
+    let NameErrors;
+    let DescriptionErrors;
+
+    if (APICategory.name === undefined) {
+      NameErrors = hasErrors('name', '');
+    } else {
+      NameErrors = hasErrors('name', APICategory.name);
+    }
+    if (NameErrors) {
+      errorText += NameErrors + '\n';
+    }
+    if (APICategory.description !== undefined) {
+      DescriptionErrors = hasErrors('description', APICategory.description);
+    }
+    if (DescriptionErrors) {
+      errorText += DescriptionErrors + '\n';
+    }
+    return errorText;
+  };
+
+  const formSaveCallback = () => {
+    const formErrors = getAllFormErrors();
+    if (formErrors !== '') {
+      console.log(formErrors);
+      Alert.error(formErrors);
+      return false;
+    } else {
+      if (id !== undefined) {
+        axios.put('/api/am/admin/api-categories/' + id, { 'name': APICategory.name, 'description': APICategory.description }, {
+          withCredentials: true,
+        }).then(() => {
+          return (
+            <FormattedMessage
+              id='AdminPages.ApiCategories.AddEdit.form.edit.successful'
+              defaultMessage='API Category edited successfully.'
+            />
+          );
+        }).catch((error) => {
+          throw error.response.body.description;
+        }).finally(() => {
+          updateList();
+        });
+      } else {
+        axios.post('/api/am/admin/api-categories/', { 'name': APICategory.name, 'description': APICategory.description }, {
+          withCredentials: true,
+        }).then(() => {
+          return (
+            <FormattedMessage
+              id='AdminPages.ApiCategories.AddEdit.form.add.successful'
+              defaultMessage='API Category added successfully.'
+            />
+          );
+        }).catch((error) => {
+          throw error.response.body.description;
+        }).finally(() => {
+          updateList();
+        });
+      }
+    }
+
   };
 
   const onChange = (e) => {
