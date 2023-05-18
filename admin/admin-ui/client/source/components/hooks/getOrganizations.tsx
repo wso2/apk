35c498d595dc0671030c9f995c01d49e305026ac
dulith <dulithcsenanayake@gmail.com>
2023-05-18 22:52:11 +0530

    Add get organization component

---
+++ admin/admin-ui/client/source/components/hooks/getOrganizations.tsx
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+type JSONValue =
+    | string
+    | number
+    | boolean
+    | { [x: string]: JSONValue }
+    | Array<JSONValue>;
+
+interface orgProps { trigger: boolean, setTrigger: (boolean) => void }
+ 
+const getOrganizations =  ({trigger, setTrigger} : orgProps ) => {
+    const [data, setData] = useState<any | null>(null);
+    const [loading, setLoading] = useState<JSONValue | null>(true);
+    const [error, setError] = useState<string>("");
+
+    const viewData = () => {
+        axios('/api/am/admin/organizations', {
+            method: 'GET',
+            withCredentials: true,
+        })
+            .then((res) => {
+                setData(res.data);
+            })
+            .catch((err) => {
+                setError(err);
+            })
+            .finally(() => {
+                setLoading(false);
+                setTrigger(false);
+            });
+    }
+    
+    useEffect(() => {
+        viewData();
+    }, []);
+
+    useEffect(() => {
+        if (trigger) {
+            viewData();
+        }
+    }, [trigger]);
+
+    return { data, loading, error };
+};
+
+export default getOrganizations;
