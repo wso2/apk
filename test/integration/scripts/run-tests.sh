9ee2f7a3afbc5c4a8ef541dd47281e9eca099ade
BLasan <benuraab@gmail.com>
2023-06-27 07:54:48 +0530

    Revert: Test script helm deployment

--- test/integration/scripts/run-tests.sh
+++ test/integration/scripts/run-tests.sh
@@ -26,7 +26,12 @@ kubectl create ns apk-integration-test
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add jetstack https://charts.jetstack.io
 helm dependency build ../../helm-charts
-helm install apk-test-setup . -n apk-integration-test \
+helm install apk-test-setup ../../helm-charts -n apk-integration-test \
+--set wso2.apk.dp.adapter.deployment.image=adapter:test \
+--set wso2.apk.dp.adapter.deployment.imagePullPolicy=IfNotPresent \
+--set wso2.apk.dp.gatewayRuntime.deployment.enforcer.image=enforcer:test \
+--set wso2.apk.dp.gatewayRuntime.deployment.enforcer.imagePullPolicy=IfNotPresent \
+--set wso2.apk.dp.configdeployer.deployment.replicas=0 \
 --set wso2.apk.dp.ratelimiter.enabled=false \
 --set wso2.apk.dp.redis.enabled=false
 
