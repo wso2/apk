b5c76a6d161a6dde726a44ec899669d103abf79c
BLasan <benuraab@gmail.com>
2023-07-13 10:53:25 +0530

    Add: Long, Date to enum values

--- adapter/internal/operator/apis/dp/v1alpha1/apipolicy_types.go
+++ adapter/internal/operator/apis/dp/v1alpha1/apipolicy_types.go
@@ -60,7 +60,7 @@ type CustomClaim struct {
 	Claim string `json:"claim,omitempty"`
 	Value string `json:"value,omitempty"`
 	// +kubebuilder:default=string
-	// +kubebuilder:validation:Enum=string;int;float;bool
+	// +kubebuilder:validation:Enum=string;int;float;bool;long;date
 	Type string `json:"type"`
 }
 
