56e553426520eecffbb719d12d83401f15c143cc
Tharsanan1 <tharsanan.15@cse.mrt.ac.lk>
2023-07-18 16:34:10 +0530

    Add resource level ratelimit tests

---
+++ test/integration/integration/tests/default-api-version-ratelimit-resource-level.go
@@ -0,0 +1,147 @@
+/*
+ *  Copyright (c) 2023, WSO2 LLC. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package tests
+
+import (
+	"testing"
+
+	"github.com/wso2/apk/test/integration/integration/utils/http"
+	"github.com/wso2/apk/test/integration/integration/utils/suite"
+)
+
+func init() {
+	IntegrationTests = append(IntegrationTests, DefaultAPIVersionRatelimitResourceLevel)
+}
+
+// DefaultAPIVersionRatelimit tests default api version feature
+var DefaultAPIVersionRatelimitResourceLevel = suite.IntegrationTest{
+	ShortName:   "DefaultAPIVersionRatelimitTest",
+	Description: "Tests an API with default version",
+	Manifests:   []string{"tests/default-api-version-ratelimit-resource-level.yaml"},
+	Test: func(t *testing.T, suite *suite.IntegrationTestSuite) {
+		ns := "gateway-integration-test-infra"
+		gwAddr := "default-api-version-ratelimit-resource-level.test.gw.wso2.com:9095"
+		token := http.GetTestToken(t)
+
+		testCases := []http.ExpectedResponse{
+			{
+				Request: http.Request{
+					Host:   "default-api-version-ratelimit-resource-level.test.gw.wso2.com",
+					Path:   "/default-api-version-ratelimit-resource-level/v2/echo",
+					Method: "GET",
+				},
+				ExpectedRequest: &http.ExpectedRequest{
+					Request: http.Request{
+						Path: "/v2/echo",
+					},
+				},
+				Backend:              "infra-backend-v1",
+				Namespace:            ns,
+				Response:             http.Response{StatusCode: 200},
+				UnacceptableStatuses: []int{429},
+			},
+			{
+				Request: http.Request{
+					Host:   "default-api-version-ratelimit-resource-level.test.gw.wso2.com",
+					Path:   "/default-api-version-ratelimit-resource-level/v1.0.0/v2/echo",
+					Method: "GET",
+				},
+				ExpectedRequest: &http.ExpectedRequest{
+					Request: http.Request{
+						Path: "/v2/echo",
+					},
+				},
+				Backend:              "infra-backend-v1",
+				Namespace:            ns,
+				Response:             http.Response{StatusCode: 200},
+				UnacceptableStatuses: []int{429},
+			},
+			{
+				Request: http.Request{
+					Host:   "default-api-version-ratelimit-resource-level.test.gw.wso2.com",
+					Path:   "/default-api-version-ratelimit-resource-level/v1.0.0/v2/echo",
+					Method: "GET",
+				},
+				ExpectedRequest: &http.ExpectedRequest{
+					Request: http.Request{
+						Path: "/v2/echo",
+					},
+				},
+				Backend:              "infra-backend-v1",
+				Namespace:            ns,
+				Response:             http.Response{StatusCode: 429},
+				UnacceptableStatuses: []int{200},
+			},
+			{
+				Request: http.Request{
+					Host:   "default-api-version-ratelimit-resource-level.test.gw.wso2.com",
+					Path:   "/default-api-version-ratelimit-resource-level/v2/echo",
+					Method: "GET",
+				},
+				ExpectedRequest: &http.ExpectedRequest{
+					Request: http.Request{
+						Path: "/v2/echo",
+					},
+				},
+				Backend:              "infra-backend-v1",
+				Namespace:            ns,
+				Response:             http.Response{StatusCode: 429},
+				UnacceptableStatuses: []int{200},
+			},
+			{
+				Request: http.Request{
+					Host:   "default-api-version-ratelimit-resource-level.test.gw.wso2.com",
+					Path:   "/default-api-version-ratelimit-resource-level/v1.0.0/v2/echo-full",
+					Method: "GET",
+				},
+				ExpectedRequest: &http.ExpectedRequest{
+					Request: http.Request{
+						Path: "/v2/echo-full",
+					},
+				},
+				Backend:              "infra-backend-v1",
+				Namespace:            ns,
+				Response:             http.Response{StatusCode: 200},
+				UnacceptableStatuses: []int{429},
+			},
+			{
+				Request: http.Request{
+					Host:   "default-api-version-ratelimit-resource-level.test.gw.wso2.com",
+					Path:   "/default-api-version-ratelimit-resource-level/v2/echo-full",
+					Method: "GET",
+				},
+				ExpectedRequest: &http.ExpectedRequest{
+					Request: http.Request{
+						Path: "/v2/echo-full",
+					},
+				},
+				Backend:              "infra-backend-v1",
+				Namespace:            ns,
+				Response:             http.Response{StatusCode: 200},
+				UnacceptableStatuses: []int{429},
+			},
+		}
+		for i := range testCases {
+			tc := testCases[i]
+			tc.Request.Headers = http.AddBearerTokenToHeader(token, tc.Request.Headers)
+			t.Run(tc.GetTestCaseName(i), func(t *testing.T) {
+				http.MakeRequestAndExpectEventuallyConsistentResponse(t, suite.RoundTripper, suite.TimeoutConfig, gwAddr, tc)
+			})
+		}
+	},
+}
--- test/integration/scripts/setup-hosts.sh
+++ test/integration/scripts/setup-hosts.sh
@@ -30,5 +30,6 @@ sudo echo "$IP disable-api-level-jwt.test.gw.wso2.com" | sudo tee -a /etc/hosts
 sudo echo "$IP disable-resource-level-jwt.test.gw.wso2.com" | sudo tee -a /etc/hosts
 sudo echo "$IP disable-api-level-jwt1.test.gw.wso2.com" | sudo tee -a /etc/hosts
 sudo echo "$IP disable-resource-level-jwt1.test.gw.wso2.com" | sudo tee -a /etc/hosts
+sudo echo "$IP default-api-version-ratelimit-resource-level.test.gw.wso2.com" | sudo tee -a /etc/hosts
 sudo echo "255.255.255.255 broadcasthost" | sudo tee -a /etc/hosts
 sudo echo "::1 localhost" | sudo tee -a /etc/hosts
