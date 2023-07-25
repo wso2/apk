3adef649ac818dfaca2b4f946f2435ec616047ef
sanoj <sanoj.punchihewa@gmail.com>
2023-07-25 10:07:43 +0530

    Increase wait time for host setup in cucumber tests

--- test/cucumber-tests/scripts/setup-hosts.sh
+++ test/cucumber-tests/scripts/setup-hosts.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 kubectl wait --timeout=5m -n apk-integration-test deployment/apk-test-setup-wso2-apk-adapter-deployment --for=condition=Available
-kubectl wait --timeout=5m -n apk-integration-test deployment/apk-test-setup-wso2-apk-gateway-runtime-deployment --for=condition=Available
+kubectl wait --timeout=15m -n apk-integration-test deployment/apk-test-setup-wso2-apk-gateway-runtime-deployment --for=condition=Available
 IP=$(kubectl get svc apk-test-setup-wso2-apk-router-service -n apk-integration-test --output jsonpath='{.status.loadBalancer.ingress[0].ip}')
 sudo echo "$IP localhost" | sudo tee -a /etc/hosts
 sudo echo "$IP idp.am.wso2.com" | sudo tee -a /etc/hosts
