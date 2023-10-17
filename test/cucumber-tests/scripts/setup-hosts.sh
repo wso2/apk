b7baa290806cb58c8b9347df0cc6df0e74ba0447
Tharsanan1 <tharsanan.15@cse.mrt.ac.lk>
2023-10-17 08:38:21 +0530

    Fix ip

--- test/cucumber-tests/scripts/setup-hosts.sh
+++ test/cucumber-tests/scripts/setup-hosts.sh
@@ -8,9 +8,10 @@ kubectl wait deployment/interceptor-service-deployment -n apk-integration-test -
 kubectl wait --timeout=5m -n apk-integration-test deployment/apk-test-setup-wso2-apk-adapter-deployment --for=condition=Available
 kubectl wait --timeout=15m -n apk-integration-test deployment/apk-test-setup-wso2-apk-gateway-runtime-deployment --for=condition=Available
 IP=$(kubectl get svc apk-test-setup-wso2-apk-gateway-service -n apk-integration-test --output jsonpath='{.status.loadBalancer.ingress[0].ip}')
+CC_IP=$(kubectl get svc apk-test-setup-wso2-apk-common-controller-web-server-service -n apk-integration-test --output jsonpath='{.status.loadBalancer.ingress[0].ip}')
 sudo echo "$IP localhost" | sudo tee -a /etc/hosts
 sudo echo "$IP idp.am.wso2.com" | sudo tee -a /etc/hosts
-sudo echo "$IP apk-test-setup-wso2-apk-common-controller-service.apk-integration-test.svc" | sudo tee -a /etc/hosts
+sudo echo "$CC_IP apk-test-setup-wso2-apk-common-controller-service.apk-integration-test.svc" | sudo tee -a /etc/hosts
 sudo echo "$IP api.am.wso2.com" | sudo tee -a /etc/hosts
 sudo echo "$IP default.gw.wso2.com" | sudo tee -a /etc/hosts
 sudo echo "$IP org1.gw.wso2.com" | sudo tee -a /etc/hosts
