c5b4d6c074cb234299852167a6342625338e8410
Tharsanan1 <tharsanan.15@cse.mrt.ac.lk>
2024-01-16 09:22:11 +0530

    Update test

--- test/integration/integration/tests/update-owner-references.go
+++ test/integration/integration/tests/update-owner-references.go
@@ -25,6 +25,8 @@ import (
 	"github.com/wso2/apk/test/integration/integration/utils/suite"
 	"k8s.io/apimachinery/pkg/types"
 	gwapiv1b1 "sigs.k8s.io/gateway-api/apis/v1beta1"
+	dpv1alpha1 "github.com/wso2/apk/common-go-libs/apis/dp/v1alpha1"
+
 )
 
 func init() {
@@ -88,47 +90,109 @@ var UpdateOwnerReferences = suite.IntegrationTest{
 		if !found {
 			t.Fatalf("Unexpected owner reference found in http route hr2:  %+v", hr2)
 		}
-		// TODO uncomment this once we figure out how to install our CRDs to the suite schema
-		// // Load backend1
-		// backend1Key := types.NamespacedName{
-		// 	Name:      "backend1",
-		// 	Namespace: namespace,
-		// }
-		// var backend1 dpv1alpha1.Backend
-		// if err := suite.Client.Get(context.TODO(), backend1Key, &backend1); err != nil {
-		// 	t.Fatalf("Unable to load backend with key %+v error: %+v", backend1Key, err)
-		// }
-		// // check the owner reference
-		// if len(backend1.ObjectMeta.OwnerReferences) != 1 || backend1.ObjectMeta.OwnerReferences[0].Name != "api1" {
-		// 	t.Fatalf("Unexpected owner reference found in backend1:  %+v", backend1)
-		// }
+		// Load backend1
+		backend1Key := types.NamespacedName{
+			Name:      "backend1",
+			Namespace: namespace,
+		}
+		var backend1 dpv1alpha1.Backend
+		if err := suite.Client.Get(context.TODO(), backend1Key, &backend1); err != nil {
+			t.Fatalf("Unable to load backend with key %+v error: %+v", backend1Key, err)
+		}
+		// check the owner reference
+		if len(backend1.ObjectMeta.OwnerReferences) != 1 || backend1.ObjectMeta.OwnerReferences[0].Name != "api1" {
+			t.Fatalf("Unexpected owner reference found in backend1:  %+v", backend1)
+		}
 
-		// // Ge backend2 check the owner reference
-		// backend2Key := types.NamespacedName{
-		// 	Name:      "backend2",
-		// 	Namespace: namespace,
-		// }
-		// var backend2 dpv1alpha1.Backend
-		// if err := suite.Client.Get(context.TODO(), backend2Key, &backend2); err != nil {
-		// 	t.Fatalf("Unable to load backend with key %+v error: %+v", backend2Key, err)
-		// }
-		// // check the owner reference
-		// if len(hr2.ObjectMeta.OwnerReferences) != 2 {
-		// 	t.Fatalf("Unexpected owner reference found in backend2:  %+v", backend2)
-		// }
-		// found = false
-		// for _, ownerRef := range hr2.ObjectMeta.OwnerReferences {
-		// 	foundLocal := false
-		// 	for _, name := range namesToCheck {
-		// 		if ownerRef.Name == name {
-		// 			foundLocal = true
-		// 		}
-		// 	}
-		// 	found = foundLocal
-		// }
-		// if !found {
-		// 	t.Fatalf("Unexpected owner reference found in backend2:  %+v", backend2)
-		// }
+		// Ge backend2 check the owner reference
+		backend2Key := types.NamespacedName{
+			Name:      "backend2",
+			Namespace: namespace,
+		}
+		var backend2 dpv1alpha1.Backend
+		if err := suite.Client.Get(context.TODO(), backend2Key, &backend2); err != nil {
+			t.Fatalf("Unable to load backend with key %+v error: %+v", backend2Key, err)
+		}
+		// check the owner reference
+		if len(hr2.ObjectMeta.OwnerReferences) != 2 {
+			t.Fatalf("Unexpected owner reference found in backend2:  %+v", backend2)
+		}
+		found = false
+		for _, ownerRef := range hr2.ObjectMeta.OwnerReferences {
+			foundLocal := false
+			for _, name := range namesToCheck {
+				if ownerRef.Name == name {
+					foundLocal = true
+				}
+			}
+			found = foundLocal
+		}
+		if !found {
+			t.Fatalf("Unexpected owner reference found in backend2:  %+v", backend2)
+		}
+
+		// Get rl1
+		rl1Key := types.NamespacedName{
+			Name:      "rl1",
+			Namespace: namespace,
+		}
+		var rl1 dpv1alpha1.RateLimitPolicy
+		if err := suite.Client.Get(context.TODO(), rl1Key, &rl1); err != nil {
+			t.Fatalf("Unable to load ratelimit with key %+v error: %+v", rl1Key, err)
+		}
+		// check the owner reference
+		if len(rl1.ObjectMeta.OwnerReferences) != 1 || rl1.ObjectMeta.OwnerReferences[0].Name != "api1" {
+			t.Fatalf("Unexpected owner reference found in ratelimit rl1:  %+v", rl1)
+		}
+
+		// Get Both apis
+		api1Key := types.NamespacedName{
+			Name:      "api1",
+			Namespace: namespace,
+		}
+		var api1 dpv1alpha1.API
+		if err := suite.Client.Get(context.TODO(), api1Key, &api1); err != nil {
+			t.Fatalf("Unable to load api with key %+v error: %+v", api1Key, err)
+		}
+
+		api2Key := types.NamespacedName{
+			Name:      "api2",
+			Namespace: namespace,
+		}
+		var api2 dpv1alpha1.API
+		if err := suite.Client.Get(context.TODO(), api2Key, &api2); err != nil {
+			t.Fatalf("Unable to load api with key %+v error: %+v", api2Key, err)
+		}
+
+		// Delete api1
+		if err := suite.Client.Delete(context.TODO(), &api1); err != nil {
+			t.Fatalf("Unable to delete api with key %+v error: %+v", api1Key, err)
+		}
+		// Wait 5 seconds
+		time.Sleep(5 * time.Second)
+		
+		// Verify hr2 has only one parent in the ownerReferences
+		if err := suite.Client.Get(context.TODO(), hr2Key, &hr2); err != nil {
+			t.Fatalf("Unable to load http route with key %+v error: %+v", hr2Key, err)
+		}
+		// check the owner reference
+		if len(hr2.ObjectMeta.OwnerReferences) != 1 {
+			t.Fatalf("Unexpected owner reference found in http route hr2:  %+v", hr2)
+		}
+		namesToCheck = []string{"api2"}
+		found = false
+		for _, ownerRef := range hr2.ObjectMeta.OwnerReferences {
+			foundLocal := false
+			for _, name := range namesToCheck {
+				if ownerRef.Name == name {
+					foundLocal = true
+				}
+			}
+			found = foundLocal
+		}
+		if !found {
+			t.Fatalf("Unexpected owner reference found in http route hr2:  %+v", hr2)
+		}
 
 	},
 }
