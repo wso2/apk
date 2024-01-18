6cf5e158810bc75cb5bb415a1ea33a8e5f97a05a
AmaliMatharaarachchi <maamalilakshika@gmail.com>
2024-01-18 18:07:24 +0530

    add gql jwt test

--- test/integration/integration/tests/gql-api.go
+++ test/integration/integration/tests/gql-api.go
@@ -35,7 +35,7 @@ var GQLAPI = suite.IntegrationTest{
 	Manifests:   []string{"tests/gql-api.yaml"},
 	Test: func(t *testing.T, suite *suite.IntegrationTestSuite) {
 		gwAddr := "gql.test.gw.wso2.com:9095"
-		// token := http.GetTestToken(t)
+		token := http.GetTestToken(t)
 
 		testCases := []http.ExpectedResponse{
 			{
@@ -70,26 +70,24 @@ var GQLAPI = suite.IntegrationTest{
 					},
 				},
 				Response: http.Response{StatusCode: 401},
+			}, {
+				Request: http.Request{
+					Host:   "gql.test.gw.wso2.com",
+					Path:   "/gql/v1",
+					Method: "POST",
+					Headers: map[string]string{
+						"Content-Type":  "application/json",
+						"Authorization": "Bearer " + token,
+					},
+					Body: `{"query":"query{\n    human(id:1000){\n        id\n        name\n    }\n    droid(id:2000){\n        name\n        friends{\n            name\n            appearsIn\n        }\n    }\n}","variables":{}}`,
+				},
+				ExpectedRequest: &http.ExpectedRequest{
+					Request: http.Request{
+						Method: "",
+					},
+				},
+				Response: http.Response{StatusCode: 200},
 			},
-			// TODO(amali) enable this test case after fixing the issue https://github.com/wso2/apk/issues/1960
-			// {
-			// 	Request: http.Request{
-			// 		Host:   "gql.test.gw.wso2.com",
-			// 		Path:   "/gql/v1",
-			// 		Method: "POST",
-			// 		Headers: map[string]string{
-			// 			"Content-Type":  "application/json",
-			// 			"Authorization": "Bearer " + token,
-			// 		},
-			// 		Body: `{"query":"query{\n    human(id:1000){\n        id\n        name\n    }\n    droid(id:2000){\n        name\n        friends{\n            name\n            appearsIn\n        }\n    }\n}","variables":{}}`,
-			// 	},
-			// 	ExpectedRequest: &http.ExpectedRequest{
-			// 		Request: http.Request{
-			// 			Method: "",
-			// 		},
-			// 	},
-			// 	Response: http.Response{StatusCode: 200},
-			// },
 		}
 		for i := range testCases {
 			tc := testCases[i]
