3523c152c2ff9516d44c96658e190d1ef2ec8a04
DinethH <dinethhettiarachchi5@gmail.com>
2024-03-11 15:40:15 +0530

    add empty test for grpc api

---
+++ test/integration/integration/tests/grpc-api.go
@@ -0,0 +1,67 @@
+/*
+ *  Copyright (c) 2023, WSO2 LLC. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package tests
+
+import (
+	"testing"
+
+	"github.com/wso2/apk/test/integration/integration/utils/http"
+	"github.com/wso2/apk/test/integration/integration/utils/suite"
+)
+
+func init() {
+	IntegrationTests = append(IntegrationTests, GRPCAPI)
+}
+
+// DisableAPISecurity test
+var GRPCAPI = suite.IntegrationTest{
+	ShortName:   "GRPCAPI",
+	Description: "Tests gRPC API",
+	Manifests:   []string{"tests/grpc-api.yaml"},
+	Test: func(t *testing.T, suite *suite.IntegrationTestSuite) {
+		gwAddr := "grpc.test.gw.wso2.com:9095"
+		//token := http.GetTestToken(t)
+
+		testCases := []http.ExpectedResponse{
+			//{
+			//	Request: http.Request{
+			//		Host:   "gql.test.gw.wso2.com",
+			//		Path:   "/gql/v1",
+			//		Method: "POST",
+			//		Headers: map[string]string{
+			//			"Content-Type": "application/json",
+			//		},
+			//		Body: `{"query":"query{\n    human(id:1000){\n        id\n        name\n    }\n}","variables":{}}`,
+			//	},
+			//	ExpectedRequest: &http.ExpectedRequest{
+			//		Request: http.Request{
+			//			Method: ""},
+			//	},
+			//	Response: http.Response{StatusCode: 200},
+			//},
+
+		}
+		for i := range testCases {
+			tc := testCases[i]
+			t.Run(tc.GetTestCaseName(i), func(t *testing.T) {
+				t.Parallel()
+				http.MakeRequestAndExpectEventuallyConsistentResponse(t, suite.RoundTripper, suite.TimeoutConfig, gwAddr, tc)
+			})
+		}
+	},
+}
