760761b5aa9554a7624b61f2f90ae347a0216e14
DinethH <dinethhettiarachchi5@gmail.com>
2024-04-16 15:24:47 +0530

    generalize testcase a bit more
    generalize testcase a bit more
    
    generalize testcase a bit more
    
    generalize testcase a bit more
    
    generalize testcase a bit more
    
    generalize testcase a bit more
    
    generalize testcase a bit more

--- test/integration/integration/tests/grpc-api.go
+++ test/integration/integration/tests/grpc-api.go
@@ -18,17 +18,10 @@
 package tests
 
 import (
-	"context"
-	"crypto/tls"
-	"github.com/wso2/apk/test/integration/integration/utils/generatedcode/student"
+	"github.com/wso2/apk/test/integration/integration/utils/grpc-code/student"
 	"github.com/wso2/apk/test/integration/integration/utils/grpcutils"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
-	"sigs.k8s.io/gateway-api/conformance/utils/config"
-	"testing"
-	"time"
-
 	"github.com/wso2/apk/test/integration/integration/utils/suite"
+	"testing"
 )
 
 func init() {
@@ -54,86 +47,15 @@ var GRPCAPI = suite.IntegrationTest{
 				},
 				ActualResponse: &student.StudentResponse{},
 				Name:           "Get Student Details",
+				Method:         student.GetStudent,
 			},
 		}
 		for i := range testCases {
 			tc := testCases[i]
 			t.Run("Invoke gRPC API", func(t *testing.T) {
 				t.Parallel()
-				invokeGRPCClientUntilSatisfied(gwAddr, t, tc, suite.TimeoutConfig, grpcutils.StudentResponseSatisfier{})
-
+				grpcutils.InvokeGRPCClientUntilSatisfied(gwAddr, t, tc, student.StudentResponseSatisfier{}, tc.Method)
 			})
 		}
 	},
-}
-
-func invokeGRPCClient(gwAddr string, t *testing.T, timeout config.TimeoutConfig) (*student.StudentResponse, error) {
-
-	t.Logf("Starting gRPC client...")
-
-	config := &tls.Config{
-		InsecureSkipVerify: true,
-	}
-	creds := credentials.NewTLS(config)
-
-	// Dial the server with the TLS credentials and a dial timeout.
-	dialCtx, dialCancel := context.WithTimeout(context.Background(), 60*time.Second)
-	defer dialCancel()
-	t.Logf("Dialing to server at %s with timeout...", gwAddr)
-	conn, err := grpc.DialContext(dialCtx, gwAddr, grpc.WithTransportCredentials(creds), grpc.WithBlock())
-	if err != nil {
-		t.Logf("Failed to connect: %v", err)
-	}
-	defer conn.Close()
-	//t.Log("Successfully connected to the server.")
-
-	c := student.NewStudentServiceClient(conn)
-
-	// Prepare the context with a timeout for the request.
-	ctx, cancel := context.WithTimeout(context.Background(), timeout.RequestTimeout)
-	defer cancel()
-
-	t.Log("Sending request to the server...")
-	// Create a StudentRequest message
-	r := &student.StudentRequest{Id: 1234}
-	response, err := c.GetStudent(ctx, r)
-	if err != nil {
-		t.Logf("Could not fetch student: %v", err)
-		t.Logf("Error: %v\n", response)
-	}
-
-	return response, nil
-}
-
-func invokeGRPCClientUntilSatisfied(gwAddr string, t *testing.T, testCase grpcutils.GRPCTestCase, timeout config.TimeoutConfig, satisfier grpcutils.ResponseSatisfier) {
-	//(delay to allow CRs to be applied)
-	time.Sleep(5 * time.Second)
-	var out *student.StudentResponse
-	var err error
-	attempt := 0
-	maxAttempts := 4
-	expected := testCase.ExpectedResponse
-	timeoutDuration := 50 * time.Second
-	for attempt < maxAttempts {
-		t.Logf("Attempt %d to invoke gRPC client...", attempt+1)
-		out, err = invokeGRPCClient(gwAddr, t, timeout)
-
-		if err != nil {
-			t.Logf("Error on attempt %d: %v", attempt+1, err)
-		} else {
-			if satisfier.IsSatisfactory(out, expected) {
-				t.Logf("Satisfactory response received: %+v", out)
-				return
-			}
-		}
-
-		if attempt < maxAttempts-1 {
-			t.Logf("Waiting %s seconds before next attempt...", timeoutDuration)
-			time.Sleep(timeoutDuration)
-		}
-		attempt++
-	}
-
-	t.Logf("Failed to receive a satisfactory response after %d attempts", maxAttempts)
-	t.Fail()
 }
