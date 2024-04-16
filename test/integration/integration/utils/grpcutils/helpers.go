3e666720824afc28487a4e376d0750abe64c7021
DinethH <dinethhettiarachchi5@gmail.com>
2024-04-16 14:35:09 +0530

    generalize testcase

--- test/integration/integration/utils/grpcutils/helpers.go
+++ test/integration/integration/utils/grpcutils/helpers.go
@@ -2,17 +2,22 @@ package grpcutils
 
 import "github.com/wso2/apk/test/integration/integration/utils/generatedcode/student"
 
-type ExpectedResponse struct {
-	Out *student.StudentResponse
-	Err error
-}
 type Request struct {
 	Host    string
 	Headers map[string]string
 }
+
+type ExpectedResponse struct {
+	Out *student.StudentResponse
+	Err error
+}
+
 type GRPCTestCase struct {
 	Request          Request
 	ExpectedResponse ExpectedResponse
 	ActualResponse   *student.StudentResponse
 	Name             string
+}
+type ResponseSatisfier interface {
+	IsSatisfactory(response interface{}, expectedResponse ExpectedResponse) bool
 }
