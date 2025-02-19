33912d3d996216562db458b82bb198d4dc191de6
Tharsanan1 <ktharsanan@gmail.com>
2025-02-19 12:11:25 +0530

    test

--- gateway/enforcer/internal/extproc/ext_proc.go
+++ gateway/enforcer/internal/extproc/ext_proc.go
@@ -144,7 +144,7 @@ func StartExternalProcessingServer(cfg *config.Server, apiStore *datastore.APISt
 	}
 }
 
-// var count = 1
+var count = 1
 
 // Process handles the external processing server stream. It continuously receives
 // requests from the stream, processes them, and sends back appropriate responses.
@@ -173,8 +173,11 @@ func (s *ExternalProcessingServer) Process(srv envoy_service_proc_v3.ExternalPro
 		default:
 		}
 		req, err := srv.Recv()
-		// count = count + 1
-		// startTime := time.Now().UnixNano()
+		count = count + 1
+		var startTime int64 
+		if count%1000 == 0 {
+			startTime = time.Now().UnixNano()
+		}
 		
 			
 		if err == io.EOF {
@@ -984,10 +987,10 @@ func (s *ExternalProcessingServer) Process(srv envoy_service_proc_v3.ExternalPro
 		if err := srv.Send(resp); err != nil {
 			//s.cfg.Logger.Sugar().Debug(fmt.Sprintf("send error %v", err))
 		}
-		// if count%10000 == 0 {
-		// 	endTime := time.Now().UnixNano()
-		// 	s.log.Info(fmt.Sprintf("Processing time: %d ns", endTime-startTime))
-		// }
+		if count%10000 == 0 {
+			endTime := time.Now().UnixNano()
+			s.log.Info(fmt.Sprintf("Processing time: %d ns", endTime-startTime))
+		}
 	}
 }
 
