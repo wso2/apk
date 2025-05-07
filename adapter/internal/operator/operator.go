c7cfbb5677a9e31cf09aaeab21bf1efc3407d3c8
Dineth <dinethhettiarachchi5@gmail.com>
2025-05-07 15:42:22 +0530

    Set cache options only if needed

--- adapter/internal/operator/operator.go
+++ adapter/internal/operator/operator.go
@@ -112,9 +112,6 @@ func InitOperator(metricsConfig config.Metrics) {
 		LeaderElection:          true,
 		LeaderElectionID:        "operator-lease.apk.wso2.com",
 		LeaderElectionNamespace: utils.GetOperatorPodNamespace(),
-		Cache: cache.Options{
-            DefaultNamespaces: defaultNSMap,
-        },
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
@@ -126,6 +123,11 @@ func InitOperator(metricsConfig config.Metrics) {
 		// if you are doing or is intended to do any operation such as perform cleanups
 		// after the manager stops then its usage might be unsafe.
 		// LeaderElectionReleaseOnCancel: true,
+	}
+	if !config.Adapter.DeployApisAtClusterLevel {
+		options.Cache = cache.Options{
+			DefaultNamespaces: defaultNSMap,
+		}
 	}
 
 	if metricsConfig.Enabled {
