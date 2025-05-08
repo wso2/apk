4d9bfb4261bbf2bacd74466fc9bdf5f819b6d65a
Dineth <dinethhettiarachchi5@gmail.com>
2025-05-08 09:50:05 +0530

    change name of values.yaml field

--- common-controller/internal/config/types.go
+++ common-controller/internal/config/types.go
@@ -41,16 +41,16 @@ type commoncontroller struct {
 	Server   server
 	Operator operator
 	// Trusted Certificates
-	Truststore        truststore
-	Environment       string
-	Redis             redis
-	Sts               sts
-	WebServer         webServer
-	InternalAPIServer internalAPIServer
-	ControlPlane      controlplane
-	Metrics           Metrics
-	Database          database
-	DeployApisAtClusterLevel bool
+	Truststore                             truststore
+	Environment                            string
+	Redis                                  redis
+	Sts                                    sts
+	WebServer                              webServer
+	InternalAPIServer                      internalAPIServer
+	ControlPlane                           controlplane
+	Metrics                                Metrics
+	Database                               database
+	DeployResourcesWithClusterRoleBindings bool
 }
 type controlplane struct {
 	Enabled             bool
