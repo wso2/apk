c9d39f41d6e2941afa4b576148abab2d3a5ea42c
Tharsanan1 <ktharsanan@gmail.com>
2025-08-06 11:34:23 +0530

    Fix setup commands

--- test/integration/scripts/setup-hosts.sh
+++ test/integration/scripts/setup-hosts.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-kubectl wait --timeout=5m -n gateway-system deployment/gateway-api-admission-server --for=condition=Available
-kubectl wait --timeout=5m -n gateway-system job/gateway-api-admission --for=condition=Complete
-kubectl wait --timeout=5m -n apk-integration-test deployment/apk-test-setup-wso2-apk-adapter-deployment --for=condition=Available
-kubectl wait --timeout=5m -n apk-integration-test deployment/apk-test-setup-wso2-apk-gateway-runtime-deployment --for=condition=Available
-IP=$(kubectl get svc apk-test-setup-wso2-apk-gateway-service -n apk-integration-test --output jsonpath='{.status.loadBalancer.ingress[0].ip}')
+# kubectl wait --timeout=5m -n gateway-system deployment/gateway-api-admission-server --for=condition=Available
+# kubectl wait --timeout=5m -n gateway-system job/gateway-api-admission --for=condition=Complete
+# kubectl wait --timeout=5m -n apk-integration-test deployment/apk-test-setup-wso2-apk-adapter-deployment --for=condition=Available
+kubectl wait --timeout=5m -n apk-integration-test --for=condition=Available -l app.kubernetes.io/component=proxy,app.kubernetes.io/managed-by=envoy-gateway deployment
+IP=$(kubectl get svc -n apk-integration-test -l app.kubernetes.io/component=proxy,app.kubernetes.io/managed-by=envoy-gateway -o jsonpath='{.items[0].status.loadBalancer.ingress[0].ip}')
 sudo echo "$IP localhost" | sudo tee -a /etc/hosts
 sudo echo "$IP all-http-methods-for-wildcard.test.gw.wso2.com" | sudo tee -a /etc/hosts
 sudo echo "$IP api-policy-with-jwt-generator.test.gw.wso2.com" | sudo tee -a /etc/hosts
@@ -44,12 +44,3 @@ sudo echo "$IP api-level-jwt.test.gw.wso2.com" | sudo tee -a /etc/hosts
 sudo echo "$IP resource-level-jwt.test.gw.wso2.com" | sudo tee -a /etc/hosts
 sudo echo "255.255.255.255 broadcasthost" | sudo tee -a /etc/hosts
 sudo echo "::1 localhost" | sudo tee -a /etc/hosts
-
-echo "AKS ratelimitter time now: "
-kubectl -n apk-integration-test exec -it $(kubectl -n apk-integration-test get pods -l app.kubernetes.io/app=ratelimiter -o jsonpath='{.items[0].metadata.name}') -- /bin/sh -c "date"
-echo "VM time now: "
-date
-echo "AKS ratelimitter time2 now: "
-kubectl -n apk-integration-test exec -it $(kubectl -n apk-integration-test get pods -l app.kubernetes.io/app=ratelimiter -o jsonpath='{.items[0].metadata.name}') -- /bin/sh -c "date"
-echo "VM time2 now: "
-date
