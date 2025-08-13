c4bf62cefec1edb8130b8c00284471a89bd0abec
Tharsanan1 <ktharsanan@gmail.com>
2025-08-13 13:34:37 +0530

    Fix bugs

--- runtime/config-deployer-service-go/cmd/main.go
+++ runtime/config-deployer-service-go/cmd/main.go
@@ -40,7 +40,7 @@ func main() {
 	cfg := config.GetConfig()
 	// logging.LoggerMain.Info("Server starting in main")
 
-	apkSchemaLocation := "resources/conf/apk-schema.json"
+	apkSchemaLocation := cfg.APKSchemaLocation
 	apkConfSchemaContent, err := os.ReadFile(apkSchemaLocation)
 	if err != nil {
 		// logging.LoggerMain.Error("Failed to read APK schema file", err)
